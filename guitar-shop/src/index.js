e6e73a9b26c2ce0dcb4642aea059b9c56e41343f
Ido Strauss <strauss02@hotmail.co.il>
2021-10-27 22:48:01 +0300

    add element selections

--- guitar-shop/src/index.js
+++ guitar-shop/src/index.js
@@ -1,5 +1,10 @@
+// SELECT ELEMENTS
 const createGuitarButton = document.querySelector('#create-guitar-button')
-
+const guitarTypeSelector = document.querySelector('#type-select')
+const guitarBrandSelector = document.querySelector('#brand-select')
+const guitarStringSelector = document.querySelector('#strings-selector')
+const guitarPriceSelector = document.querySelector('#price-selector')
+//
 function getRes() {
   axios
     .get('http://localhost:8080/')
