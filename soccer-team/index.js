7c7ee6e34d2de6ea30e8757bd1fa36fb1a7e69d6
Ido Strauss <strauss02@hotmail.co.il>
2021-10-27 15:20:26 +0300

    add class

--- soccer-team/index.js
+++ soccer-team/index.js
@@ -6,9 +6,59 @@
   \/_____/   \/_____/   \/_____/   \/_____/   \/_/ /_/   \/_____/ 
                                                                   
  ______   ______                                                  
-/\  ___\ /\  ___\                      coded by strauss02
-\ \  __\ \ \ \____                        for cyber4s training program         
+/\  ___\ /\  ___\                   coded by strauss02
+\ \  __\ \ \ \____                   for the cyber4s training program         
  \ \_\    \ \_____\                                               
   \/_/     \/_____/                                               
                                                                                       
  /*************************************************************************/
+
+import crypto from 'crypto'
+
+/**
+    "...Create a class that represents a Person..."
+  */
+class Person {
+  firstName
+  surName
+  salary
+  age
+  #id
+  constructor(firstName, surName, salary, age) {
+    this.firstName = firstName
+    this.surName = surName
+    this.salary = salary
+    this.age = age
+    this.id = crypto.randomBytes(2).toString('hex')
+  }
+
+  //GETTERS
+  get firstName() {
+    return this.firstName
+  }
+  get surName() {
+    return this.surName
+  }
+  get salary() {
+    return this.salary
+  }
+  get age() {
+    return this.age
+  }
+
+  //SETTERS
+  set salary(val) {
+    this.salary = val
+  }
+  set age(val) {
+    this.age = val
+  }
+}
+
+const baby = new Person('Jimmy', 'Chickpea', `5$`, 2)
+console.log(baby)
+console.log(baby.salary)
+
+/**
+ *
+ */
